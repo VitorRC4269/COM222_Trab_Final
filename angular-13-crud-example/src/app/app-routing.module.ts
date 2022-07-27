eedf8116d8f1c892106083432316aa1af99d282a
VitorRC4269 <vitorramosdecarvalho@gmail.com>
2022-07-26 22:42:44 -0300

    fim maybe

--- angular-13-crud-example/src/app/app-routing.module.ts
+++ angular-13-crud-example/src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { AddAssociadoComponent } from './components/add-associado/add-associado.component';
 import { AddFuncionarioComponent } from './components/add-funcionario/add-funcionario.component';
 import { AddPublicacaoComponent } from './components/add-publicacao/add-publicacao.component';
@@ -19,9 +16,7 @@ import { FuncionarioLogoutComponent } from './components/funcionario-logout/func
 
 const routes: Routes = [
   { path: '', redirectTo: 'associadoLogin', pathMatch: 'full' },
-  { path: 'tutorials', component: TutorialsListComponent },
-  { path: 'tutorials/:id', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent },
+ 
   { path: 'addAssociado', component: AddAssociadoComponent },
   { path: 'addFuncionario', component: AddFuncionarioComponent },
   { path: 'addPublicacao', component: AddPublicacaoComponent },
